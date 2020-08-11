28e4b2161cc083777604da83ab6a157584555501
Michelle Stermitz <stermitzm@gmail.com>
2020-08-11 14:55:51 -0700

    Removed hardcoded geodata

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
+const request = require('superagent');
 const express = require('express');
-var cors = require('cors');
-const geoData = require('./data/geo.js');
+const cors = require('cors');
 const weatherData = require('./data/weather.js');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 app.use(cors());
+
+const { GEOCODE_API_KEY } = process.env;
 
 function getWeather(lat, lon) {
     const data = weatherData.data;
@@ -21,8 +25,14 @@ function getWeather(lat, lon) {
     return forecastArray;
 }
 
-function getLatLong(cityName) {
-    const city = geoData[0];
+// https://api.weatherbit.io/v2.0/forecast/daily?&lat=38.123&lon=-78.543&key={api-key}
+// https://us1.locationiq.com/v1/search.php?key={api-key}&q={city-name}&format=json
+
+
+async function getLatLong(cityName) {
+    const response = await request.get(`https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${cityName}&format=json`);
+
+    const city = response.body[0];
 
     return {
         formatted_query: city.display_name,
@@ -31,11 +41,11 @@ function getLatLong(cityName) {
     };
 }
 
-app.get('/location', (req, res) => {
+app.get('/location', async(req, res) => {
     try {
         const userInput = req.query.search;
     
-        const mungedData = getLatLong(userInput);
+        const mungedData = await getLatLong(userInput);
         res.json(mungedData);
     } catch (e) {
         res.status(500).json({ error: e.message });
